1ae18bf96a63a457cd1b61f5deb1086b9cbd612d
MaxKiv <max@kivits.top>
2026-08-18 15:17:55 +0200

    wip(TMTM-42): RT thread attempt

---
+++ motor-controller/src/lib.rs
@@ -0,0 +1,43 @@
+pub mod cia402;
+pub mod cmd;
+pub mod handshake;
+pub mod rt;
+pub mod setpoint;
+
+use anyhow::bail;
+use tokio::sync::{mpsc, watch};
+
+use crate::{
+    cia402::Cia402Manager, cmd::MotorCommand, handshake::HandshakeManager, rt::RealTimeSetpoint,
+};
+
+pub struct MotorController {
+    rx: mpsc::Receiver<MotorCommand>,
+    handshake_manager: HandshakeManager,
+    cia402_manager: Cia402Manager,
+    tx: watch::Sender<RealTimeSetpoint>,
+}
+
+impl MotorController {
+    pub async fn run(&mut self) -> anyhow::Result<()> {
+        loop {
+            if let Some(cmd) = self.rx.recv().await {
+                match cmd {
+                    MotorCommand::Halt => todo!(),
+                    MotorCommand::Disable => todo!(),
+                    MotorCommand::Enable => todo!(),
+                    MotorCommand::Home => todo!(),
+                    MotorCommand::Cia402TransitionTo(s) => {
+                        if let Err(err) = self.cia402_manager.try_transition_to(s) {}
+                    }
+                    MotorCommand::Move(default_motor_setpoint) => todo!(),
+                    MotorCommand::EnterCyclicMode(cyclic_mode) => todo!(),
+                    MotorCommand::ExitCyclic(default_mode) => todo!(),
+                    MotorCommand::MoveCyclic(_) => todo!(),
+                }
+            } else {
+                bail!("MotorController: command rx channel closed")
+            }
+        }
+    }
+}
